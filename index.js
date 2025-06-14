325c7e2c5cc40a05a079a4e2b645ad2f5a0a4100
moaaz <moaazabobakr69@gmail.com>
2025-06-14 21:27:23 +0300

    handling payments

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const env = require("dotenv").config();
 const clientRoute = require("./routes/ClientRoutes");
+const paymentRoute = require("./routes/paymentRoute");
 const PORT = process.env.PORT;
 const cors = require("cors");
 
@@ -10,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use("/join", clientRoute);
-
+app.use("/payments", paymentRoute);
 mongoose
   .connect("mongodb://127.0.0.1:27017/LeapTech")
   .then(() => console.log("connected to DB"))
